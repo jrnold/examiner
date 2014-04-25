c141c2658a7e8460d5eb166b2d6f40091c36353c
jrnold <jeffrey.arnold@gmail.com>
2014-04-25 13:26:23 -0400

    rewrite backend: new classes, format functions

--- R/examiner-package.R
+++ R/examiner-package.R
@@ -4,6 +4,8 @@
 #' @docType package
 #' @import yaml
 #' @import stringr
+#' @import whisker
+#' @import plyr
 NULL
 
 beginenv <- function(x) str_c("\\begin{", x, "}")
@@ -18,7 +20,8 @@ ltxnewenv <- function(x, begin = "", end = "") {
       ltxnewenv("problemtext", "\\par", ""),
       ltxnewenv("problemset", "\\par", ""),
       ltxnewenv("problems", "\\par", ""),
-      ltxnewenv("problemsettext", "\\par", ""),
+      ltxnewenv("problemsetpretext", "\\par", ""),
+      ltxnewenv("problemsetposttext", "\\par", ""),
       "\\newlist{answers}{enumerate}{1}",
       "\\setlist[answers]{label=(\\alph*)}")
 
@@ -29,20 +32,41 @@ ltxnewenv <- function(x, begin = "", end = "") {
 #' @return A character vector with the header
 #' @export
 examiner_latex_header <- function() {
-    str_c(.EXMINER_LATEX_HEADER, collapse = "\n")
+    str_c(.EXAMINER_LATEX_HEADER, collapse = "\n")
 }
+
+
+#' Create an answerlist object
+#' @param x A data frame with colums: text, correct, and fixed.
+#' @return An object of class \code{answerlist}.
+#' @export
+answerlist <- function(x) {
+    x <- as.data.frame(x)
+    class(x) <- c("answerlist", class(x))
+    x
+}
+
+#' @export
+format.answerlist <- function(x, show_solutions = FALSE, ...) {
+    format_answer <- function(text, correct, ...) {
+        str_c("\\item", text, sep = " ")
+    }
+    str_c(maply(x, format_answer, .expand = FALSE, .drop = TRUE), 
+          collapse = "\n")
+}
+
 
 #' Create a problem object
 #'
-#' @param x \code{character} vector with the prompt for the problem.
+#' @param text \code{character} vector with the prompt for the problem.
 #' @param answers A \code{list} of \code{problem} or \code{problemset} objects.
 #' @param correct Indices of answers which are correct.
 #' @param first Number of observations at the beginning of \code{answers} which will not be shuffled.
 #' @param last Number of observations at the end of \code{answers} which will not be shuffled.
 #' @param randomizable Can the answers be shuffled?
 #' @return An object of class \code{problem}.
-#' @export
-problem <- function(x,
+#' @keywords internal 
+problem <- function(text = "",
                     answers = character(),
                     solution = "",
                     correct = 1L, first = 0L, last = 0L,
@@ -52,13 +76,13 @@ problem <- function(x,
     last <- min(c(last, n))
 
     firsti <- seq_len(first)
-    lasti <- pmin(rev(length(x) - seq_len(last) + 1),
+    lasti <- pmin(rev(length(answers) - seq_len(last) + 1),
                   length(answers))
     answers <-
-        data.frame(text = answers,
-                   correct = seq_along(answers) == correct,
-                   fixed = seq_along(answers) %in% c(firsti, lasti),
-                   stringsAsFactors = FALSE)
+        answerlist(data.frame(text = answers,
+                              correct = seq_along(answers) == correct,
+                              fixed = seq_along(answers) %in% c(firsti, lasti),
+                              stringsAsFactors = FALSE))
     .Data <-
         list(text = text,
              answers = answers,
@@ -72,21 +96,14 @@ problem <- function(x,
 }
 
 shuffle_answers <- function(x) {
-    indices <- seq_len(nrow(x))[x[["fixed"]]]
+    indices <- seq_len(nrow(x))[!x[["fixed"]]]
     iorder <- sample(indices, length(indices))
     x[indices, ] <- x[iorder, , drop = FALSE]
     attr(x, "order") <- iorder
+    x
 }
 
-format_answer <- function(text, correct, ...) {
-    str_c("\\item", text, sep = " ")
-}
-
-format_answerlist <- function(x) {
-    str_c(mlply(x[["answers"]], format_answer), collapse = "\n")
-}
-
-format_problem <- function(x) {
+format_problem <- function(x, show_solutions = FALSE) {
     str_c(beginenv("problem"),
           beginenv("problemtext"),
           str_c(x[["text"]], sep = "\n"),
@@ -99,41 +116,41 @@ format_problem <- function(x) {
 }
 
 #' @export
-format.problem <- function(x, randomize = FALSE) {
+format.problem <- function(x, randomize = FALSE, show_solutions = FALSE, ...) {
     if (x[["randomizable"]] && as.logical(randomize)) {
         x[["answers"]] <- shuffle_answers(x[["answers"]])
     }
-    x[["answers"]] <- format_answerlist(x[["answers"]])
-    format_problem(x)
+    x[["answers"]] <- format(x[["answers"]], show_solutions = show_solutions)
+    format_problem(x, show_solutions = show_solutions)
 }
 
 #' Create a problemset object
 #'
-#' @param x \code{character} vector with the prompt for the problemset.
-#' @param problems list of problems or other problemsets
+#' @param problems list of problems
+#' @param pretext \code{character} vector with text to go before the questions.
+#' @param posttext \code{character} vector with text to go after the questions.
 #' @return A \code{problemset} object
 #' @export
 problemset <- function(problems, pretext = "", posttext = "") {
-    if (! inherits(problems, "list")) {
-        stop("problems must be a list")
+    if (! inherits(problems, "list") ||
+        !all(sapply(problems, inherits, what = "problem"))) {
+        stop("problems must be a list of problem objects")
     }
-    if (!all(sapply(problems, inherits, what = "examiner"))) {
-        stop("problems must be a list of problem or problemset objects")
-    }
-    .Data <- list(problems, pretext = pretext, posttext = posttext)
+    .Data <- list(problems = problems, pretext = pretext,
+                  posttext = posttext)
     class(.Data) <- c("problemset", "list")
     .Data
 }
 
 format_problems <- function(problems, ...) {
-    str_c(ldply(problems, format, ...), collapse = "\n")
+    str_c(laply(problems, format, ...), collapse = "\n")
 }
 
-format_problemset <- function(x) {
+format_problemset <- function(x, show_solutions = FALSE) {
     str_c(beginenv("problemset"),
           beginenv("problemsetpretext"),
           str_c(as.character(x[["pretext"]]), collapse = "\n"),
-          endenv("problemsettext"),
+          endenv("problemsetpretext"),
           beginenv("problems"),
           x[["problems"]],
           endenv("problems"),
@@ -145,9 +162,9 @@ format_problemset <- function(x) {
 }
 
 #' @export
-format.problemset <- function(x, randomize = FALSE, ...) {
-    x[["problems"]] <- format_problems(x, randomize = randomize)
-    format_problemset(x)
+format.problemset <- function(x, randomize = FALSE, show_solutions = FALSE, ...) {
+    x[["problems"]] <- format_problems(x[["problems"]], show_solutions = show_solutions, randomize = randomize)
+    format_problemset(x, show_solutions = show_solutions)
 }
 
 #' Create problemset from a yaml file
